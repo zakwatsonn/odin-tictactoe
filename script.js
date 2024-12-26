451121c7d2297462a34c65b6ac93e081dbb40424
zakwatsonn <168912087+zakwatsonn@users.noreply.github.com>
2024-12-26 14:08:05 +0000

    create board in gameBoard fac

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function gameBoard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
 
+    //creating board using nested loops
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(0);
+        }
+    }
+
+    console.log(board);
 };
 
 function player() {
